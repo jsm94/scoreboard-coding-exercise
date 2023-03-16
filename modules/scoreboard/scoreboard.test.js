e06acfb9627611d5331f7612ddb6c6fb53778869
jsm94 <smjuan24@gmail.com>
2023-03-16 20:58:45 +0100

    Make first red test green

--- modules/scoreboard/scoreboard.test.js
+++ modules/scoreboard/scoreboard.test.js
@@ -1,5 +1,7 @@
 import { describe, expect, it } from 'vitest'
 
+class ScoreBoard {}
+
 describe('Score Board should', () => {
     it('create an empty score board', () => {
         const scoreBoard = new ScoreBoard()
