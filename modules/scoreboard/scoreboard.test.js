554e29b46c78cc6378ac8bfa0dd997019ffb68be
jsm94 <smjuan24@gmail.com>
2023-03-16 21:14:59 +0100

    Refactor first test and get it to pass

--- modules/scoreboard/scoreboard.test.js
+++ modules/scoreboard/scoreboard.test.js
@@ -1,9 +1,35 @@
 import { describe, expect, it } from 'vitest'
+/**
+ * @interface
+ */
+class IScoreBoard {
+    /**
+     * @type {Object[]}
+     */
+    games
 
-class ScoreBoard {}
+    /**
+     * Get all games from scoreboard
+     */
+    getGames() {}
+}
+
+/**
+ * Class that implements a scoreboard
+ * @implements {IScoreBoard}
+ */
+class ScoreBoard extends IScoreBoard {
+    constructor() {
+        super()
+        this.games = []
+    }
+
+    getGames = () => this.games
+}
 
 describe('Score Board should', () => {
     it('create an empty score board', () => {
         const scoreBoard = new ScoreBoard()
+        expect(scoreBoard.getGames().length).toBe(0)
     })
 })
